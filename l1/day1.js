a034f5c6356698f2bc38ba67c66d9e4aea47f235
Saurabh Gupta <saurabhprof523416@gmail.com>
2025-09-05 01:03:48 +0530

    first commit

---
+++ l1/day1.js
@@ -0,0 +1,12 @@
+// A React Component is just a function that returns JSX
+function App() {
+  return (
+    <div>
+      <h1>Hello React - Day 1 🚀</h1>
+      <p>This is my first React component.</p>
+    </div>
+  );
+}
+
+// Always export your component so it can be used in other files
+export default App;
