e45fadbfdae5a34244db76544be1f506d07bb342
Saurabh Gupta <saurabhprof523416@gmail.com>
2025-09-05 11:13:24 +0530

    updated index.html with new content

--- l1/day1.js
+++ l1/day1.js
@@ -1,12 +1,5 @@
-// A React Component is just a function that returns JSX
-function App() {
-  return (
-    <div>
-      <h1>Hello React - Day 1 🚀</h1>
-      <p>This is my first React component.</p>
-    </div>
-  );
-}
-
-// Always export your component so it can be used in other files
-export default App;
+const header1=document.createElement('h1');
+header1.innerText="hello";
+header1.style.backgroundColor="pink";
+header1.style.fontSize="pink";
+header1.style.color="white";
